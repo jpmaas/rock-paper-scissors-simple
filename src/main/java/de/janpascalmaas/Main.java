d5ba3bba10018626ecfd02967919b27563d30f20
Jan Pascal Maas <jan.pascal.maas@posteo.de>
2025-07-26 16:52:17 +0200

    Connect all domains with each other and run the game

--- src/main/java/de/janpascalmaas/Main.java
+++ src/main/java/de/janpascalmaas/Main.java
@@ -1,17 +1,34 @@
 package de.janpascalmaas;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import de.janpascalmaas.domain.Game;
+import de.janpascalmaas.domain.Player;
+import de.janpascalmaas.domain.shape.Shape;
+import de.janpascalmaas.domain.shape.ShapeType;
+import de.janpascalmaas.domain.strategy.OneShapeStrategy;
+import de.janpascalmaas.domain.strategy.RandomStrategy;
+import de.janpascalmaas.service.ConsoleOutputService;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Player paperPlayer = Player.builder()
+                .withName("A")
+                .withStrategy(new OneShapeStrategy(new Shape(ShapeType.PAPER)))
+                .build();
+
+        Player randomPlayer = Player.builder()
+                .withName("B")
+                .withStrategy(new RandomStrategy(new Shape(ShapeType.ROCK),
+                                                  new Shape(ShapeType.PAPER),
+                                                  new Shape(ShapeType.SCISSORS)))
+                .build();
+
+        Game game = Game.builder()
+                .addPlayer1(paperPlayer)
+                .addPlayer2(randomPlayer)
+                .numberOfRounds(100)
+                .play();
+
+        new ConsoleOutputService().printGame(game);
     }
 }
