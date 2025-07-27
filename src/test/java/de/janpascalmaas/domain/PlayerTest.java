e7170fa2d375e289f33e11d224a6123a479f8927
Jan Pascal Maas <jan.pascal.maas@posteo.de>
2025-07-27 10:30:03 +0200

    Add missing PlayerTest

--- src/test/java/de/janpascalmaas/domain/PlayerTest.java
+++ src/test/java/de/janpascalmaas/domain/PlayerTest.java
@@ -1,7 +1,55 @@
 package de.janpascalmaas.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import de.janpascalmaas.domain.strategy.PlayerStrategy;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
 
 class PlayerTest {
 
+    @Test
+    void whenStrategyIsNull_thenThrowsException() {
+        assertThatThrownBy(() -> Player.builder()
+                .withName("Bob")
+                .withStrategy(null)
+                .build())
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Player strategy must not be null");
+    }
+
+    @Test
+    void whenNameIsNull_thenThrowsException() {
+        assertThatThrownBy(() -> Player.builder()
+                .withName(null)
+                .withStrategy(mock(PlayerStrategy.class))
+                .build())
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Player name must not be null or blank");
+    }
+
+    @Test
+    void whenNameIsBlank_thenThrowsException() {
+        assertThatThrownBy(() -> Player.builder()
+                .withName("   ")
+                .withStrategy(mock(PlayerStrategy.class))
+                .build())
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Player name must not be null or blank");
+    }
+
+    @Test
+    void whenTheScoreIsIncrementedThenItIsRecordedCorrectly() {
+        Player given = Player.builder()
+                .withName("Alice")
+                .withStrategy(mock(PlayerStrategy.class))
+                .build();
+
+        assertThat(given.getScore()).isZero();
+
+        given.incrementScore();
+
+        assertThat(given.getScore()).isOne();
+    }
 }
